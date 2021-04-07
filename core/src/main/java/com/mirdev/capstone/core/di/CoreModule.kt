421cd4e6d0d05a0db8b58fc57c4054c670111275
telkomdev-mirza <mirzauksyh@gmail.com>
2021-04-07 18:09:10 +0700

    Add database encryption & certificate pinning

--- core/src/main/java/com/mirdev/capstone/core/di/CoreModule.kt
+++ core/src/main/java/com/mirdev/capstone/core/di/CoreModule.kt
@@ -10,6 +10,9 @@ import com.mirdev.capstone.core.data.source.remote.network.ApiService
 import com.mirdev.capstone.core.domain.repository.IMovieRepository
 import com.mirdev.capstone.core.utils.AppExecutors
 import com.mirdev.capstone.core.utils.CustomInterceptors
+import net.sqlcipher.database.SQLiteDatabase
+import net.sqlcipher.database.SupportFactory
+import okhttp3.CertificatePinner
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.android.ext.koin.androidContext
@@ -20,21 +23,27 @@ import java.util.concurrent.TimeUnit
 
 val databaseModule = module {
     factory { get<MovieDatabase>().movieDao() }
+    val passphrase: ByteArray = SQLiteDatabase.getBytes("dicoding".toCharArray())
+    val factory = SupportFactory(passphrase)
     single {
         Room.databaseBuilder(
             androidContext(),
             MovieDatabase::class.java, "Movie.db"
-        ).fallbackToDestructiveMigration().build()
+        ).fallbackToDestructiveMigration().openHelperFactory(factory).build()
     }
 }
 
 val networkModule = module {
     single {
+        val certificatePinner = CertificatePinner.Builder()
+            .add("*.themoviedb.org", "sha256/+vqZVAzTqUP8BGkfl88yU7SQ3C8J2uNEa55B7RZjEg0=")
+            .build()
         OkHttpClient.Builder()
             .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
             .addInterceptor(CustomInterceptors())
             .connectTimeout(120, TimeUnit.SECONDS)
             .readTimeout(120, TimeUnit.SECONDS)
+            .certificatePinner(certificatePinner)
             .build()
     }
     single {
